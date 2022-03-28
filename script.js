748682504d946021c6093cdf516afbbe147af606
Josue Alamo <josuealamo95@gmail.com>
2022-03-28 15:03:32 -0500

    Added event listener that changes the squares' back ground color to yellow when mouse is over it.

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 container = document.querySelector(".container");
-    row = document.createElement("div");
-    row.classList.add("row")
-    container.appendChild(row)
- 
         
 
 function createSquare() {
-    square = document.createElement("div");
+    let square = document.createElement("div");
     square.classList.add("square-css");
     row.appendChild(square);
 }
 
-function createSixteenSquares() {
-    for (i=0; i < 16; i++) {
-        createSquare();
+function createGrid(n) {
+    for (i=0; i < n; i++) {
+        row = document.createElement("div");
+        row.classList.add("row")
+        container.appendChild(row)
+
+        for (j=0; j < n; j++) {
+            createSquare();
+        }
     }
 }
 
-createSixteenSquares();
+createGrid(16);
+
+let squares = document.querySelectorAll('.square-css');
+
+squares.forEach(square => square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'yellow';
+    }
+));
+
+
+
+
+
+//['event1', 'event2'].forEach(event => square.addEventListener(event, doSomething));
+
