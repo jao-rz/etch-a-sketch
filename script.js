04a7c877b475b670e18fcd8d8f8966e9b771bda4
Josue Alamo <josuealamo95@gmail.com>
2022-03-30 13:08:19 -0500

    Removed 'mouse grab' default behavior that happens when holding mouse down and moving it.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 container = document.querySelector(".container");
         
-
 function createSquare() {
     let square = document.createElement("div");
     square.classList.add("square-css");
@@ -19,18 +18,28 @@ function createGrid(n) {
     }
 }
 
+
+let isDrawing = false;
+
 createGrid(16);
 
 let squares = document.querySelectorAll('.square-css');
 
-squares.forEach(square => square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = 'yellow';
-    }
-));
+container.addEventListener('mousedown', e => {
+    e.preventDefault();
+    isDrawing = true;
+});
 
+squares.forEach(square => square.addEventListener('mousemove', e => {
+    if (isDrawing === true) {
+        square.style.backgroundColor = 'yellow';
+    };
+}));
 
-
-
-
-//['event1', 'event2'].forEach(event => square.addEventListener(event, doSomething));
-
+container.addEventListener('mouseup', e => {
+    e.preventDefault();
+    if (isDrawing === true) {
+        isDrawing = false;
+    };
+    console.log("mouse is up")
+});
